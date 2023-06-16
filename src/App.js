642fa8dbb0652c3fc7ddaed0c4c9fcc5e6bb8617
Airis Wang <airis.wang2014@fgvmail.br>
2023-06-15 20:52:22 -0400

    updated the toggle feature to update the state of each task

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
-
 const App = () => {
+  const [taskData, setTaskData] = useState(
+    [
+      {
+        id: 1,
+        title: 'Mow the lawn',
+        isComplete: false,
+      },
+      {
+        id: 2,
+        title: 'Cook Pasta',
+        isComplete: true,
+      },
+  ]);
+
+  const updateTaskData = (id) => {
+    setTaskData(tasks => {
+      return tasks.map(task => {
+        if (id === task.id) {
+          return {
+            ...task,
+            isComplete: !task.isComplete,
+          };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div><TaskList tasks={TASKS} /></div>
+        <div>
+          <TaskList tasks={taskData} onUpdateTaskData={updateTaskData} />
+        </div>
       </main>
     </div>
   );
